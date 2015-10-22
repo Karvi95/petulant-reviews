9c2cdf0435fcd1b48119f7b1957f79fd6517fd3c
Arvindram Krishnamoorthy <karvi90@yahoo.com>
2015-10-22 11:35:16 -0700

    Parse not adding

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# petulant-reviews
+#petulant-reviews
+Fourth challenge for the info343 course
+
+Did you receive help from any other sources (classmates, etc.)? If so, please list who.
+
+
+
+Did you complete any advanced extensions to this challenge? If so, what?
+
+
+
+Approximately how many hours did it take you to complete this challenge?
+
+
+
+Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better?
+
+While I greatly appreciate how often you are available on Slack, as well as the general caliber of your helpful comments, I think having office hours sometime before the Monday's when the assignments are due would be great; you might even not need to be THAT active on Slack because people can ask better questions in pursue/ learn ways to debug for themselves? Maybe? If it's a hassle, obviously ignore this suggestion :D .
---
+++ js/review.js
@@ -0,0 +1,84 @@
+// Initialize Parse app
+Parse.initialize('njJAclkNheZu7lDJTbvCqvqPX0n4KGdoTPEDT0R5', 'C88cKJ2Lms975Xx59oRTq5ULOtbrKhemDtFUrhCS');
+
+// Create a new sub-class of the Parse.Object, with name "Multiverse"
+var Multiverse = Parse.Object.extend('Multiverse');
+
+// Click event when form is submitted
+$('form').submit(function() {
+
+    // Create a new instance of your Multiverse class 
+    var plane = new Multiverse();
+
+    // For each input element, set a property of your new instance equal to the input's value
+    
+    $(this).find('input').each(function(){
+        plane.set($(this).attr('id'), $(this).val());
+        $(this).val('');
+    })
+
+    // After setting each property, save your new instance back to your database
+    plane.save(null, {
+        success:getData
+    })
+    return false
+})
+
+// Write a function to get data
+var getData = function() {
+
+    // Set up a new query for our Music class
+    var query = new Parse.Query(Multiverse)
+
+    // Set a parameter for your query -- where the website property isn't missing
+    query.notEqualTo('plane', '')
+
+    /* Execute the query using ".find".  When successful:
+        - Pass the returned data into your buildList function
+    */
+    query.find({
+        success:function(results) {
+            buildList(results)
+        } 
+    })
+}
+
+// A function to build your list
+var buildList = function(data) {
+    // Empty out your ordered list
+    $('ol').empty()
+
+    // Loop through your data, and pass each element to the addItem function
+    data.forEach(function(d){
+        addItem(d);
+    })
+}
+
+// This function takes in an item, adds it to the screen
+var addItem = function(item) {
+    // Get parameters (color, plane, hotspot) from the data item passed to the function
+    var color = item.get('color')
+    var plane = item.get('plane')
+    var hotspot = item.get('hotspot')
+    
+    // Append li that includes text from the data item
+    var li = $('<li>Check out ' + plane + ', their best hotspot is ' + hotspot + '</li>')
+    
+    // Create a button with a <span> element (using bootstrap class to show the X)
+    var button = $('<button class="btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span></button>')
+    
+    // Click function on the button to destroy the item, then re-call getData
+    button.click(function() {
+        item.destroy({
+            success:getData
+        })
+    })
+
+    // Append the button to the li, then the li to the ol
+    li.append(button);
+    $('ol').append(li)
+    
+}
+
+// Call your getData function when the page loads
+getData()
